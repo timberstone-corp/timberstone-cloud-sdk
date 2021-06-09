5ddeb1f2086778fb8fa9b5dc101734662087d7c1
Marcelo Ventura <marceloventura@outlook.com>
2021-06-09 14:54:56 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# timberstone-cloud-sdk
-Pulumi python package to manage Cloud Infrastructure following T.C.T. best standards.
+# Timberstone Cloud SDK
+
+Pulumi python package to manage Cloud Infrastructure following T.C.T. best standards. By using the cloud sdk users will benefit from an abstraction layer to deploy approved cloud rescources. This SDK is presented to you by T.C.T. (Timberstone Cloud Team), please open an issue here if you find any problems.
+
+
+## Steps to build
+
+```
+python3 -m pip install --upgrade build
+python3 -m build
+```
+
+Reference: [Python Packaging Projects](https://packaging.python.org/tutorials/packaging-projects/) 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="timberstone-cloud-sdk",  # Replace with your own username
+    version="0.0.2",
+    author="Marcelo Ventura",
+    author_email="marceloventura@outlook.com",
+    description="A pyhton pulumi sdk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/venturamarcelo/timberstone-cloud-sdk",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
